81fc61507e6d1d954c8a4f26eab4e0662084375f
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-04-16 16:20:30 -0400

    additional styling

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,28 +1,63 @@
-
+import {Table, TableContainer, TableCell, TableHead, TableRow, TableBody, Box} from "@material-ui/core";
+import mainStyles from './Main.module.scss';
 
 export default function Main (props) {
   
   const {meal, ingredients} = props;
 
-  return ( 
-    <>
-      <header>
-        <h1> {meal.strMeal} </h1>
-      </header>
+  const ytube = meal.meals[0].strYoutube.replace("watch?v=", "embed/")
 
-      <div> 
-        <h1>Ingredients</h1>
-        <ul>
-          {ingredients.map((ing, i) => {
-            return (
-            <li key = {i}>
-              <span> {ing.name} </span>
-              <span> {ing.amount} </span>
-            </li>
-          )
-          })}
-        </ul>
+  return (
+    <>
+      <h1 className={mainStyles.main__title}> {meal.meals[0].strMeal} </h1>
+      <div className={mainStyles.main}>
+        <TableContainer className={mainStyles.main__table}>
+          <Table
+            style={{
+              margin: "0 auto",
+              border: "2px solid #1A5276"  
+            }}
+          >
+            <TableHead>
+              <TableRow style={{ backgroundColor: "#1A5276" }}>
+                <TableCell style={{ color: "#fff" }} align="center">
+                  <p className={mainStyles.main__table__name} >Ingredients</p> 
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ingredients.map((ing, i) => {
+                return (
+                  <TableRow key={ing.name}>
+                    <TableCell align="left" style ={{display:"flex", gap: "0.5rem"}}> <p className={mainStyles.main__table__rows} > <span> {ing.amount}</span> {ing.name} </p> </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box className={mainStyles.main__head}>
+          <div className={mainStyles.main__video}>
+            <iframe
+              id="ytplayer"
+              className={mainStyles.main__iframe}
+              type="text/html"
+              src={`${ytube}`}
+            ></iframe>
+          </div>
+
+          <div className={mainStyles.main__instruction}>
+            <h1 className={mainStyles.main__instruction__title}>
+              Instructions
+            </h1>
+            {meal.meals[0].strInstructions.split("\\r\\n").map((ele, i) => {
+              return (
+                <p className={mainStyles.main__instruction__text}> {ele}</p>
+              );
+            })}
+          </div>
+        </Box>
       </div>
     </>
-  )
+  );
 } 
